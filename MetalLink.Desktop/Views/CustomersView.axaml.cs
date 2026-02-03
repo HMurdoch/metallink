a0963d236138c6c8d508818efe211b39deb8dcdf
Hugh Murdoch <hughmurdoch@gmail.com>
2026-02-03 16:08:30 +0200

    refactor(customers): improve search results DataGrid and auto-edit on selection
    CHANGES TO SEARCH RESULTS:
    - Remove Edit button from action buttons in DataGrid
    - Auto-trigger Edit command when customer is selected from results
    - Remove empty column header (was blank)
    - Rename Actions column header for clarity
    - Set DataGrid width to 60% (three-fifths) instead of 100%
    - Align DataGrid to left side
    
    BEHAVIOR:
    - User searches for and selects a customer from results
    - Automatically triggers edit view for selected customer
    - Log Ticket and Delete buttons remain in Actions column
    - Results DataGrid is now more compact (60% width)

--- MetalLink.Desktop/Views/CustomersView.axaml.cs
+++ MetalLink.Desktop/Views/CustomersView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using MetalLink.Desktop.ViewModels;
 
 namespace MetalLink.Desktop.Views;
 
@@ -8,6 +9,23 @@ public partial class CustomersView : UserControl
     public CustomersView()
     {
         InitializeComponent();
+        DataContextChanged += (s, e) =>
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                viewModel.PropertyChanged += (sender, args) =>
+                {
+                    if (args.PropertyName == nameof(viewModel.FoundCustomer) && viewModel.FoundCustomer != null)
+                    {
+                        // Trigger edit when a customer is selected from results
+                        if (viewModel.EditCustomerCommand.CanExecute(viewModel.FoundCustomer))
+                        {
+                            viewModel.EditCustomerCommand.Execute(viewModel.FoundCustomer);
+                        }
+                    }
+                };
+            }
+        };
     }
 
     private void InitializeComponent()
