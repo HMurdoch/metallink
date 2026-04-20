c744b53eb71eeeffd378e6f97917afb5a585c70a
Hugh Murdoch <hughmurdoch@gmail.com>
2026-04-20 12:04:18 +0200

    feat(desktop): Implement Prices view UI and interactive DataGrid

---
+++ MetalLink.Desktop/Views/PriceRowTooltipView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MetalLink.Desktop.Views;
+
+public partial class PriceRowTooltipView : UserControl
+{
+    public PriceRowTooltipView()
+    {
+        Avalonia.Markup.Xaml.AvaloniaXamlLoader.Load(this);
+    }
+}
